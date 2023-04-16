5c724fd15ac6dd1ea605bae2ab36c652f5f62946
manoelacmn <manoel.acmn@puccampinas.edu.br>
2023-04-16 16:56:30 -0300

    before gpt

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,16 @@
-import * as functions from "firebase-functions";
+import * as functions from "../node_modules/firebase-functions";
+
+
+export const uwu = functions.https.onRequest((request, response) => {
+  console.log("Hello!");
+  response.end("UWU");
+});
+
+
+exports.myFunction = functions.firestore
+  .document("/emergency")
+  .onWrite((context) => { 
+  });
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
